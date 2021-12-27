2408ef894b1397e9c9c254cd863106821ef11324
Matias Micenmacher <matamicen@gmail.com>
2021-12-27 16:02:41 -0300

    Atomic (oracle,gt,vt) working without checks

--- first.js
+++ first.js
@@ -1,6 +1,7 @@
 const { logicSigFromByte } = require('algosdk');
 const algosdk = require('algosdk');
 const fs = require('fs');
+const utils = require('./utils');
 
 
 
@@ -189,17 +190,17 @@ async function logicSigcTransaction() {
         console.log('lastRound: '+status["last-round"])  
         lastvalid = status["last-round"] + 10
 
-let delegateTemplate = `
+let oracleTemplate = `
 #pragma version 3
 txn TypeEnum
 int pay
 ==
 global GroupSize
-int 1
+int 3
 ==
 &&
 txn Receiver
-addr HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA
+addr YEUJW5EPVUDGXYG67LWCL376GMHYKORJECSB2JAW5WY4ESL3CEHPRSEWX4
 ==
 &&
 txn CloseRemainderTo
@@ -225,24 +226,99 @@ int <lastvalid>
 &&
 return
 `;
-        delegateTemplate = delegateTemplate.replace("<min>", parseInt("00205000"));    
-        let program = delegateTemplate.replace("<lastvalid>", lastvalid); 
+
+let gt_logicsig = `
+#pragma version 3
+txn TypeEnum
+int pay
+==
+global GroupSize
+int 3
+==
+&&
+txn CloseRemainderTo
+global ZeroAddress
+==
+&&
+txn RekeyTo
+global ZeroAddress
+==
+&&
+txn Fee
+int 1000
+<=
+&&
+return
+`;
+
+let ct_logicsig = `
+#pragma version 3
+txn TypeEnum
+int pay
+==
+global GroupSize
+int 3
+==
+&&
+txn CloseRemainderTo
+global ZeroAddress
+==
+&&
+txn RekeyTo
+global ZeroAddress
+==
+&&
+txn Fee
+int 1000
+<=
+&&
+gtxn 0 Sender
+addr BE5RNUTEERA3PJW6SRDS4NOQ4S6UVXRUPN2YOTJLGUROXCCXADDVWRSJR4
+==
+&&
+return
+`;
+        let compilation = await compileProgram(algodClient, ct_logicsig);
+        oracleProgramReferenceProgramBytesReplace = Buffer.from(compilation.result, "base64");
+        let program_array = new Uint8Array (oracleProgramReferenceProgramBytesReplace);
+        let args = null;
+        // let lsig = algosdk.makeLogicSig(program_array, args);
+        ct_lsig = new algosdk.LogicSigAccount(program_array, args);
+        console.log('CT_logicsic_account: '+ct_lsig.address())
+
+
+        compilation = await compileProgram(algodClient, gt_logicsig);
+        oracleProgramReferenceProgramBytesReplace = Buffer.from(compilation.result, "base64");
+        program_array = new Uint8Array (oracleProgramReferenceProgramBytesReplace);
+        args = null;
+        // let lsig = algosdk.makeLogicSig(program_array, args);
+        gt_lsig = new algosdk.LogicSigAccount(program_array, args);
+        console.log('GT_logicsic_account: '+gt_lsig.address())
+
+
+
+
+
+
+
+        oracleTemplate = oracleTemplate.replace("<min>", parseInt("00205000"));    
+        let program = oracleTemplate.replace("<lastvalid>", lastvalid); 
         console.log(program)
-        let compilation = await compileProgram(algodClient, program);
+        compilation = await compileProgram(algodClient, program);
         //generate unique filename
         // let uintAr = _base64ToArrayBuffer(compilation.result);
         console.log(compilation.result)
         oracleProgramReferenceProgramBytesReplace = Buffer.from(compilation.result, "base64");
         console.log(oracleProgramReferenceProgramBytesReplace)
-        let program_array = new Uint8Array (oracleProgramReferenceProgramBytesReplace);
-        let args = null;
+        program_array = new Uint8Array (oracleProgramReferenceProgramBytesReplace);
+        args = null;
         // let lsig = algosdk.makeLogicSig(program_array, args);
-        let lsig = new algosdk.LogicSigAccount(program_array, args);
-        console.log('after logicsigAccount: '+lsig.address())
+        oracle_lsig = new algosdk.LogicSigAccount(program_array, args);
+        console.log('after logicsigAccount: '+oracle_lsig.address())
 
         let oracle_sk = algosdk.mnemonicToSecretKey("popular sauce pride off fluid you come coffee display list stadium blood scout bargain segment laptop hand employ demise grass sign adult want abstract exhibit")
         console.log(oracle_sk.addr.toString())
-        lsig.sign(oracle_sk.sk);
+        oracle_lsig.sign(oracle_sk.sk);
 
         let accountInfo = await algodClient.accountInformation(oracle_sk.addr).do();
         // let accountInfo = await algodClient.accountInformation(myAccount2).do();
@@ -261,56 +337,64 @@ return
          await keypress();
  
          // receiver defined as TestNet faucet address 
-         const receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
+         receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
          const enc = new TextEncoder();
         //  const note = enc.encode("5");
          tipoCambio = parseInt("00205000") // tipo de cambio, 4 ultimos son decimales
-         const note = algosdk.encodeUint64(tipoCambio);
-         let amount = 3000000;
+         note = algosdk.encodeUint64(tipoCambio);
+         let amount = 0;
          let closeout = receiver; //closeRemainderTo
          let sender = oracle_sk.addr.toString();
-         let txn = algosdk.makePaymentTxnWithSuggestedParams(sender, receiver, amount, undefined, note, params);
+        
+         // oracle tx1
+         let txn1 = algosdk.makePaymentTxnWithSuggestedParams(sender, sender, amount, undefined, note, params);
 
 
            // sign transaction with logic signature
 
-            const lstx = algosdk.signLogicSigTransactionObject(txn, lsig);
+            // const lstx = algosdk.signLogicSigTransactionObject(txn1, lsig);
 
             // send transaction (it should fail because of the logic signature, which returns 0)
-            console.log('Sending transaction...');
-            tx = await algodClient.sendRawTransaction(lstx.blob).do();
-            console.log(tx);
+            // console.log('Sending transaction...');
+            // tx = await algodClient.sendRawTransaction(lstx.blob).do();
+            // console.log(tx);
+
+        // GT paga a CT tx0
+        sender = gt_lsig.address(); // Green Treassury Account
+        receiver = ct_lsig.address(); // CT account
+        amount = 2000000;
+        note = enc.encode("GT paga");
+        let txn0 = algosdk.makePaymentTxnWithSuggestedParams(sender, receiver, amount, undefined, note, params);
+
+        // CT paga a GT tx2
+        sender = ct_lsig.address(); // Green Treassury Account
+        receiver = gt_lsig.address() // GT account
+        amount = 1000000;
+        note = enc.encode("CT paga");
+        let txn2 = algosdk.makePaymentTxnWithSuggestedParams(sender, receiver, amount, undefined, note, params);
+
+          // assign group id to transactions
+        algosdk.assignGroupID([txn0, txn1, txn2]);
+
+        // sign transactions
+        // const stxn0 = txn0.signTxn(gt_lsig.sk);
+        const stxn0 = algosdk.signLogicSigTransactionObject(txn0, gt_lsig);
+        const stxn1 = algosdk.signLogicSigTransactionObject(txn1, oracle_lsig);
+        const stxn2 = algosdk.signLogicSigTransactionObject(txn2, ct_lsig);
+
+        // send transactions (note that the accounts need to be funded for this to work)
+        console.log('Sending transactions...');
+        const { txId } = await algodClient.sendRawTransaction([stxn0.blob, stxn1.blob, stxn2.blob]).do();
+
+        console.log("atomic:" + txId)
+
+                // wait for confirmation – timeout after 2 rounds
+        console.log('Awaiting confirmation (this will take several seconds)...');
+        const roundTimeout = 2;
+        await utils.waitForConfirmation(algodClient, txId, roundTimeout);
+        console.log('Transactions successful.');
+
       
-         // Sign the transaction
-        //  let signedTxn = txn.signTxn(oracle_sk.sk);
-        //  let txId = txn.txID().toString();
-        //  console.log("Signed transaction with txID: %s", txId);
- 
-         // Submit the transaction
-        //  await algodClient.sendRawTransaction(signedTxn).do();
-
-        // la signa quien firmo logicsig o sea oracleAccount (YEUJW5EPVUDGXYG67LWCL376GMHYKORJECSB2JAW5WY4ESL3CEHPRSEWX4)
-        // let rawSignedTxn = algosdk.signLogicSigTransaction(txn, lsig);
-        // let oracleTxSigned = algosdk.signLogicSigTransactionObject(txn, lsig);
-        // console.log('rawSignedTxn: '+rawSignedTxn)
-        // let tx = algodClient.sendRawTransaction(oracleTxSigned.blob);
-        // console.log("Transaction lsig : " + tx.txId);
-
- 
-        //  // Wait for confirmation
-        //  let confirmedTxn = await waitForConfirmation(algodClient, txId, 4);
-        //  //Get the completed Transaction
-        //  console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
-        //  // let mytxinfo = JSON.stringify(confirmedTxn.txn.txn, undefined, 2);
-        //  // console.log("Transaction information: %o", mytxinfo);
-        //  var string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
-        //  console.log("Note field: ", string);
-        //  accountInfo = await algodClient.accountInformation(oracle_sk.addr).do();
-        //  console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);        
-        //  console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
-        //  let closeoutamt = startingAmount - confirmedTxn.txn.txn.amt - confirmedTxn.txn.txn.fee;        
-        //  console.log("Close To Amount: %d microAlgos", closeoutamt);
-        //  console.log("Account balance: %d microAlgos", accountInfo.amount);
     }
         catch (err) {
             console.log("err", err);
